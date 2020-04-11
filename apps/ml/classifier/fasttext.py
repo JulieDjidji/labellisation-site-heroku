e53e49fa38aeba98328feef94a0bef6ba70872a4
JulieDjidji <julie.djiriguian@gmail.com>
2020-04-11 23:23:46 +0200

    Fix fasttext loading

--- apps/ml/classifier/fasttext.py
+++ apps/ml/classifier/fasttext.py
@@ -10,6 +10,11 @@ import numpy as np
 import random
 import time
 
+import subprocess
+wdir=os.getcwd()
+os.chdir(wdir+'/fastText')
+subprocess.call('pip install -q fasttext', shell=True)
+os.chdir(wdir)
 import fasttext
 
 class FasttextClassifier:
